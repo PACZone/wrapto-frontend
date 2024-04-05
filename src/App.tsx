452a48ed9dfa999502738778fddd2155e9378937
Hamed Ahmadi <hamed.tkd.1381@gmail.com>
2024-04-05 12:20:03 +0330

    init:init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ReactLenis } from "@studio-freight/react-lenis";
+
+export default function App() {
+  return (
+    <ReactLenis root>
+      <main>
+        <h1 className="text-3xl font-bold">Hello Wrapto!</h1>
+      </main>
+    </ReactLenis>
+  );
+}
