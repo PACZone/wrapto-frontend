651c7da40819128fb876e808a2afe8db7c87d92d
Hamed Ahmadi <hamed.tkd.1381@gmail.com>
2024-04-22 18:11:33 +0330

    fix: Remove loading from first loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,31 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 import { routes } from "./routes";
 import { Route, Routes } from "react-router-dom";
-import Loading from "./components/Loading";
 import { Header } from "./layout/Header";
-import { useEffect, useState } from "react";
 import Footer from "layout/Footer";
 import ScrollToTop from "components/ScrollToTop";
 import "./App.css";
 
 export default function App() {
-
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-
-        const handleLoadingTime = () => {
-            setTimeout(() => {
-                setLoading(false);
-            }, 3700);
-        };
-
-        handleLoadingTime();
-    }, []);
-
-    useEffect(() => {}, []);
     return (
         <ReactLenis root>
             <ScrollToTop />
-            <Loading />
-            {!loading && (
-                <>
-                    <Header />
-                    <Routes>
-                        {Object.keys(routes).map((route, index) => {
-                            const routeKey = route as keyof typeof routes;
-                            return (
-                                <Route
-                                    key={index}
-                                    element={routes[routeKey].element}
-                                    path={routes[routeKey].path}
-                                />
-                            );
-                        })}
-                    </Routes>
-                    <Footer />
-                </>
-            )}
+            <>
+                <Header />
+                <Routes>
+                    {Object.keys(routes).map((route, index) => {
+                        const routeKey = route as keyof typeof routes;
+                        return (
+                            <Route
+                                key={index}
+                                element={routes[routeKey].element}
+                                path={routes[routeKey].path}
+                            />
+                        );
+                    })}
+                </Routes>
+                <Footer />
+            </>
         </ReactLenis>
     );
 }
