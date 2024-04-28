9dfa567cc91f9e254df1e81a2eff7e31d659fc88
Hamed Ahmadi <hamed.tkd.1381@gmail.com>
2024-04-29 00:08:58 +0330

    fix: fix transfer box input

--- src/wagmi/networks.ts
+++ src/wagmi/networks.ts
@@ -14,7 +14,7 @@ export const networks : Record<string,NetworkT>={
   amoy:{
     name: 'MATIC',
     contractAddress:'0x56D09Db604B815E4322CA825D5EB10a6a9938d92',
-    explorer:"https://www.okx.com/explorer/amoy/tx",
+    explorer:"https://amoy.polygonscan.com/tx",
     rpcUrl:"https://polygon-amoy.infura.io/v3/ebf2aa9568884ba0aceb39bc35377102",
     chainId:80002,
     symbol:"MATIC"
